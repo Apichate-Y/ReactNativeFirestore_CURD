38f70f361ca061ff3b09f86eb9b899b012554e6d
LoukGolf89 <apichate.yatra@gmail.com>
2021-01-14 18:32:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>I'm Mobile Developer</Text>
+    </View>
+  );
+}
+
+export default App;
